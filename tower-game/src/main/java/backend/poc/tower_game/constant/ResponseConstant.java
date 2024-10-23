1601d09bb6a0751b88d739001761e40bedf59324
phoolego <42689414+phoolego@users.noreply.github.com>
2024-10-23 17:50:00 +0700

    Delete tower-game directory

--- tower-game/src/main/java/backend/poc/tower_game/constant/ResponseConstant.java
+++
@@ -1,8 +0,0 @@
-package backend.poc.tower_game.constant;
-
-public class ResponseConstant {
-    private ResponseConstant() {}
-
-    public static final String SUCCESS_CODE = "success";
-    public static final String GENERAL_ERROR_CODE = "general_error";
-}
